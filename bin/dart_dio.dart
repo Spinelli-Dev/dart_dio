f6dd5923286eefb1d50b995ae9eb401eb543e311
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-18 10:26:20 -0300

    Primeiro Commit

---
+++ bin/dart_dio.dart
@@ -0,0 +1,5 @@
+import 'package:dart_dio/dart_dio.dart' as dart_dio;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_dio.calculate()}!');
+}
---
+++ test/dart_dio_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_dio/dart_dio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
