98d175288329f0f8c96dbfb101966fc652fcf5c0
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-18 10:42:17 -0300

    Formas de declarar variáveis

--- bin/dart_dio.dart
+++ bin/dart_dio.dart
@@ -1,5 +1,22 @@
 import 'package:dart_dio/dart_dio.dart' as dart_dio;
 
+
+//comentário
 void main(List<String> arguments) {
-  print('Hello world: ${dart_dio.calculate()}!');
+  // Formas de declarar variáveis
+  String texto = "Meu texto";
+  var texto2 = "Meu texto";
+  String texto3;
+
+  int numero = 1;
+  var numero2 = 1;
+  int numero3;
+
+  List<String> lista = [];
+  lista.add("A");
+  var lista2 = [];
+  lista2.add("A");
+  lista2.add(1);
+  lista2.add(2.5);
+
 }
