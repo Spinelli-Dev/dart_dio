4f5a82c25b2fa7c03aa379776a7c35d5cd19bee3
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-18 10:59:31 -0300

    Formas de comparar

--- bin/dart_dio.dart
+++ bin/dart_dio.dart
@@ -1,22 +1,74 @@
 import 'package:dart_dio/dart_dio.dart' as dart_dio;
 
-
-//comentário
 void main(List<String> arguments) {
-  // Formas de declarar variáveis
-  String texto = "Meu texto";
-  var texto2 = "Meu texto";
-  String texto3;
+// // Formas de comparar
+// int numero1 = 10;
+// var numero2 = 11;
 
-  int numero = 1;
-  var numero2 = 1;
-  int numero3;
+// print("Retorna verdadeiro se e somente se esse inteiro for par");
+// print(numero1.isEven);
+// print(numero2.isEven);
 
-  List<String> lista = [];
-  lista.add("A");
-  var lista2 = [];
-  lista2.add("A");
-  lista2.add(1);
-  lista2.add(2.5);
+// print("Retorrna verdadeiro se e somente se este inteiro for ímpar");
+// print(numero1.isOdd);
+// print(numero2.isOdd);
+
+// print("Retorna se o número é finito");
+// print(numero1.isFinite);
+
+// print("Retorna se o número é infinito");
+// print(double.infinity);
+
+// print("Retorna se é um número inválido");
+// print(numero1.isNaN);
+
+// print("Retorna se o número é negativo");
+// print((numero1 * -1).isNegative);
+
+// // Conversão de tipos
+// print("Converte String para Int");
+// print(int.parse("10"));
+// print(int.parse("teste")); //se não conseguir, vai dar erro.
+// print(int.tryParse("teste")); // melhor opção, pois não trava o software
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // // Formas de declarar variáveis
+  // String texto = "Meu texto";
+  // var texto2 = "Meu texto";
+  // String texto3;
+
+  // int numero = 1;
+  // var numero2 = 1;
+  // int numero3;
+
+  // List<String> lista = [];
+  // lista.add("A");
+  // var lista2 = [];
+  // lista2.add("A");
+  // lista2.add(1);
+  // lista2.add(2.5);
 
 }
