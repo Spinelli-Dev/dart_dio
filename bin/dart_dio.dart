ec45d8184443bceb150c29d4855a9c97324df731
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-18 11:09:18 -0300

    Tabalhando com double

--- bin/dart_dio.dart
+++ bin/dart_dio.dart
@@ -1,6 +1,47 @@
 import 'package:dart_dio/dart_dio.dart' as dart_dio;
 
 void main(List<String> arguments) {
+// // Trabalhando com double
+// double numero1 = 10.1;
+// var numero2 = 11.1;
+
+// print("Remove ponto flutuante");
+// print(numero1.truncate());
+
+// print("Converte para inteiro.");
+// print(numero1.toInt());
+
+// print("Arredonda para cima.");
+// print(numero1.ceil());
+
+// print("Arredonda para baixo.");
+// print(numero1.floor());
+
+// print("Converte String para double");
+// print(double.parse("10"));
+// print(double.parse("teste"));
+// print(double.tryParse("teste"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // // Formas de comparar
 // int numero1 = 10;
 // var numero2 = 11;
