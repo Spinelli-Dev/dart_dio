9f83e8a54806d5a4811f614e7a0b332035bb8137
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-25 14:59:32 -0300

    Calculadora com IF

--- bin/dart_dio.dart
+++ bin/dart_dio.dart
@@ -5,6 +5,64 @@ import 'dart:convert';
 import 'package:dart_dio/dart_dio.dart' as dart_dio;
 
 void main(List<String> arguments) {
+  // // Criando uma Calculadora
+  // print("Bem vindo a nossa calculadora!");
+  // print("Informe o primeiro número:");
+  // var line = stdin.readLineSync(encoding: utf8);
+  // var num1 = double.parse(line ?? "0");
+
+  // print("Informe o segundo número:");
+  // line = stdin.readLineSync(encoding: utf8);
+  // var num2 = double.parse(line ?? "0");
+
+  // print("Informe a operação: (+ , - , * , /)");
+  // line = stdin.readLineSync(encoding: utf8);
+  // var operacao = line ?? "";
+
+  // // Utilizando o método IF
+  // double resultado = 0;
+  // if (operacao == "+") {
+  //   resultado = num1 + num2;
+  // } else if (operacao == "-") {
+  //   resultado = num1 - num2;
+  // } else if (operacao == "*") {
+  //   resultado = num1 * num2;
+  // } else if (operacao == "/") {
+  //   resultado = num1 / num2;
+  // } else {
+  //   print("Operação inválida.");
+  //   exit(0);
+  // }
+
+  // print(resultado);
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
   // // Lendo dados do Console
   // print("Infome a primeira nota: ");
   // var line = stdin.readLineSync(encoding: utf8);
